64c9a1a394be4b4f3a4cf31e8330258f2d112226
Ahmed Rashad <ahm.rashad.202@gmail.com>
2025-08-25 04:38:16 +0300

    Render posts with javascript

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const posts = [
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        avatar: "public/avatar-vangogh.jpg",
+        post: "public/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -12,8 +12,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: "public/avatar-courbet.jpg",
+        post: "public/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -21,10 +21,42 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "public/avatar-ducreux.jpg",
+        post: "public/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
 ]
+
+const container = document.getElementById("container")
+
+function createPost({name, username, location, avatar,post,comment,likes}) {
+    return `
+    <section class="post">
+        <div class="post-header">
+          <img class="post-avatar" src="${avatar}">
+          <div>
+            <p class="bold">${name}</p>
+            <p>${location}</p>
+          </div>
+        </div>
+        <img class="post-img" src="${post}">
+        <div class="icon-container">
+          <img class="icon" src="public/icon-heart.png">
+          <img class="icon" src="public/icon-comment.png">
+          <img class="icon" src="public/icon-dm.png">
+        </div>
+        <p class="bold">${likes} likes</p>
+        <p><span class="bold">${username}</span> ${comment}</p>
+      </section>
+    `
+}
+
+function renderPost(post) {
+    container.innerHTML += createPost(post)
+}
+
+for (let i = 0; i < posts.length; i++) {
+    renderPost(posts[i])
+}
 
